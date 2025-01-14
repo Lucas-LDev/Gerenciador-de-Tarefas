353e82cbcf05b6ea2cca80ff62ded5dc699ea7e1
Lucas-LDev <lucasunisinos0@gmail.com>
2025-01-14 18:47:11 -0300

    feat: add functionality to save tasks in browser with localStorage

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//função de adicionar tarefas na ul
+//criando um array de tarefas
+let tasks = [];
+
+//salvando o array de tarefas em JSON mesmo que o array esteja vazio
+tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+//exibindo tarefas
+createTask();
+
+//função de adicionar tarefas no array
 function addTask() {
     //capturando a caixa da message
     let message = document.getElementById("message");
@@ -10,21 +19,50 @@ function addTask() {
 
     //verificação de tarefa vazia
     if (!task) {
+        //trocando o texto da mensagem em caso de erro
         message.textContent = "A tarefa não pode ser nula";
+        
         //trocando a cor do texto para rosa
         message.style.color = "#FF79C6";
         showMessage();
     } else {
-        //pegando a ul(lista de tarefas)
-        let ul = document.getElementById("tasks-ul");
+        //adicionando a tarefa no array de tarefas
+        tasks.push({"name": task, "status": false});
 
+        //salvando o nome e o estado da tarefa no array de tarefas
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        //trocando cor do texto para verde
+        message.style.color = "#00ff40";
+        showMessage();
+
+        createTask();
+    }
+    //limpando o input ao adicionar uma tarefa
+    taskInput.value = "";
+}
+
+//função de criar as tarefas
+function createTask() {
+    //pegando a ul(lista de tarefas)
+    let ul = document.getElementById("tasks-ul");
+
+    //limpando a ul para não duplicar toda vez que recarregar
+    ul.innerHTML = "";
+
+    tasks.forEach(t => {
         //criando a li(tarefa)
         let li = document.createElement("li");
         li.classList.add("task-li");
 
         //criando o span para o texto da tarefa
         let taskSpan = document.createElement("span");
-        taskSpan.textContent = task[0].toUpperCase() + task.slice(1);
+        taskSpan.textContent = t.name[0].toUpperCase() + t.name.slice(1);
+
+        //adicionando a classe "completed" se a tarefa estiver marcada como concluída anteriormente
+        if (t.status) {
+            li.classList.add("completed");
+        }
 
         //criando o botão de concluir tarefa
         let btnComplete = document.createElement("button");
@@ -33,16 +71,24 @@ function addTask() {
         //marcando como concluído
         btnComplete.addEventListener("click", () => {
             li.classList.toggle("completed");
+
+            //atualizando o status da tarefa, nesse caso, ele apenas inverte o valor do status
+            t.status = !t.status;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
         })
 
         //criando o botão de apagar tarefa
         let btnRemove = document.createElement("button");
         btnRemove.classList.add("btn-remove");
         btnRemove.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-        //removendo a tarefa da ul ao apagar
+        //removendo a tarefa da ul ao clicar no botão de apagar
         btnRemove.addEventListener("click", () => {
             li.classList.toggle("removing");
             li.remove();
+
+            //atualizando o array e adicionando nele somente as tasks que forem diferente da qual eu cliquei no botão de excluir
+            tasks = tasks.filter(task => task !== t);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
         })
 
         //adicionando o span e os botões no li
@@ -51,13 +97,7 @@ function addTask() {
         li.appendChild(btnRemove);
         //adicionando o li na ul
         ul.appendChild(li);
-
-        //trocando cor do texto para verde
-        message.style.color = "#00ff40";
-        showMessage();
-    }
-    //limpando o input ao adicionar uma tarefa
-    taskInput.value = "";
+    });
 }
 
 //função de adicionar tarefas pressionando a tecla enter
